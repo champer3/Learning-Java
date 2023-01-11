3e529dff82b8c8c481020c40e17b9a1b0c58b50d
champer3 <okunolas21@gmail.com>
2023-01-10 20:53:51 -0600

    working with variables

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        int age = 30;
+        age = 69;
+        System.out.println("Hello world");
+        System.out.println(age);
+    }
+}
