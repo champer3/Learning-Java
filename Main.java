32e4c357dd3d8ec76d1feea407d8bf3fdecae685
champer3 <okunolas21@gmail.com>
2023-01-12 23:11:53 -0600

    pointless commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void main(String[] args) {
         int age = 30;
         age = 69;
+        age = 77;
         System.out.println("Hello world");
         System.out.println(age);
     }
